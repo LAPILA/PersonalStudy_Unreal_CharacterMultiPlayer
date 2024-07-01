677052253407279d910cd43e64e1945587cbccf9
LAPILA <lapila3004@gmail.com>
2024-07-01 21:34:28 +0900

    InitSetting

---
+++ Source/CharacterMultiPlayer/CharacterMultiPlayer.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "CharacterMultiPlayer.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, CharacterMultiPlayer, "CharacterMultiPlayer" );
+ 
