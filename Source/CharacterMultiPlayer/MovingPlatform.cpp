cef6fb16687d3a9543c76f0dec214d62fafe5134
LAPILA <lapila3004@gmail.com>
2024-07-09 20:56:38 +0900

    Week1_2
    Server Complete

--- Source/CharacterMultiPlayer/MovingPlatform.cpp
+++ Source/CharacterMultiPlayer/MovingPlatform.cpp
@@ -21,24 +21,48 @@ void AMovingPlatform::BeginPlay()
 		//액터의 움직임이 서버에서 클라로 복제됨
 		SetReplicateMovement(true);
 	}
+	GlobalStartLocation = GetActorLocation();
+	GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
 }
 
-void AMovingPlatform::Tick(float DeltaTime) 
+void AMovingPlatform::Tick(const float DeltaTime) 
 {
 	Super::Tick(DeltaTime);
 
-	if (HasAuthority()) {
-		//틱당 위치를 받고
-		FVector Location = GetActorLocation();
-		FVector GlobalTargetLocation = GetTransform().TransformPosition(TargetLocation);
-		//틱당 방향을 받고
-		//숫자가 작을때 보호해주는 SafeNormal
-		FVector Direction = (GlobalTargetLocation - Location).GetSafeNormal();
-		// 속도 * 초당시간 * 방향
-		Location += Speed * DeltaTime * Direction;
-		//해당 위치를 재설정
-		SetActorLocation(Location);
+	if(ActiveTriggers > 0)
+	{
+		if (HasAuthority()) {
+			//틱당 위치를 받고
+			FVector Location = GetActorLocation();
+			float JourneyLength = (GlobalTargetLocation - GlobalStartLocation).Size();
+			float JourneyTravelled = (Location - GlobalStartLocation).Size();
+
+			if (JourneyTravelled > JourneyLength)
+			{
+				FVector Swap = GlobalStartLocation;
+				GlobalStartLocation = GlobalTargetLocation;
+				GlobalTargetLocation = Swap;
+			}
+			//숫자가 작을때 보호해주는 SafeNormal
+			FVector Direction = (GlobalTargetLocation - GlobalStartLocation).GetSafeNormal();
+			// 속도 * 초당시간 * 방향
+			Location += Speed * DeltaTime * Direction;
+			//해당 위치를 재설정
+			SetActorLocation(Location);
+		}
 	}
 }
 
+void AMovingPlatform::AddActiveTrigger()
+{
+	ActiveTriggers++;
+}
+
+void AMovingPlatform::RemoveActiveTrigger()
+{
+	if (ActiveTriggers > 0)
+	{
+		ActiveTriggers--;
+	}
+}
 
---
+++ Source/CharacterMultiPlayer/PuzzlePlatformsGameInstance.cpp
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformsGameInstance.h"
+#include "Engine/Engine.h"
+
+UPuzzlePlatformsGameInstance::UPuzzlePlatformsGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Construct"));
+}
+
+void UPuzzlePlatformsGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Init"));
+}
+
+void UPuzzlePlatformsGameInstance::Host()
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0,1.0f,FColor::Green,TEXT("Hosting"));
+
+	UWorld* World = GetWorld();
+	if (!ensure(Engine != nullptr)) return;
+	World->ServerTravel("/Game/PuzzlePlatforms/Maps/ThirdPersonMap?listen");
+}
+
+void UPuzzlePlatformsGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5.0f, FColor::Green, FString::Printf(TEXT("Joining %s"),*Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, TRAVEL_Absolute);
+}
+
+
